88d121cdbb5d85f280b50b5ba752f4354173702e
SamuelJJk <samueljean.j24@gmail.com>
2024-04-05 15:02:03 -0400

    cat pic and fact added

--- fetch.mjs
+++ fetch.mjs
@@ -1,11 +1,18 @@
 //fetch request
 const body = document.querySelector('body')
+const CPContainer = document.querySelector('.catPic') 
+const CFContainer = document.querySelector('.catFact')
+
+import { randNum} from "./func.mjs"
 
 export async function getCatPic(){
     const reponse = await fetch("https://api.thecatapi.com/v1/images/search")
     const catPic = await reponse.json();
-    console.log(catPic);
-    console.log(catPic[0].url);
+    // console.log(catPic);
+    // console.log(catPic[0].url);
+    let image = document.createElement('img');
+    image.setAttribute('src', catPic[0].url)
+    CPContainer.appendChild(image)
 
 }
 
@@ -14,6 +21,7 @@ export async function catFacts() {
         .then((reponse) => reponse.json())
         .then((json) => {
             console.log(json)
+            CFContainer.append(json.data[randNum()].fact)
         })
     .catch ((error) => {
         console.log('error with fetch')
--- func.mjs
+++ func.mjs
@@ -1,1 +1,8 @@
 //classes and function 
+
+export function randNum (){
+    let num  = Math.floor(Math.random()*10)
+    return num
+    
+}
+randNum()
